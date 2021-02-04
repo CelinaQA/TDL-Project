74634ac4410d67243dcf0167a3fc054caab0315d
Celina <cbasa@qa.com>
2021-02-04 15:29:55 +0000

    Created Spring Project for TDL Project

---
+++ src/main/java/com/qa/demo/CelinaTdlApplication.java
@@ -0,0 +1,13 @@
+package com.qa.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelinaTdlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CelinaTdlApplication.class, args);
+	}
+
+}
