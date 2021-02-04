dbe31919987605c679175d05b6cc3479d1b7b4b6
Celina <cbasa@qa.com>
2021-02-04 18:55:16 +0000

    Modified application-dev.properties to connect to MySQL database (used Maven -> update project to register active profile)

--- src/main/java/com/qa/demo/CelinaTdlApplication.java
+++ src/main/java/com/qa/demo/CelinaTdlApplication.java
@@ -1,5 +1,7 @@
 package com.qa.demo;
 
+import java.time.LocalTime;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,7 @@ public class CelinaTdlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CelinaTdlApplication.class, args);
+		System.out.println("The time is: "+LocalTime.now());
 	}
 
 }
