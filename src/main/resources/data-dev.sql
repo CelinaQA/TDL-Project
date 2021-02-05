53d65cbfdc7ddcbb5d0608a97a527dd2184f3865
Celina <cbasa@qa.com>
2021-02-05 15:56:40 +0000

    Modified application-dev.properties file to allow initialization of data-dev.sql and added .sql files to resources folder

---
+++ src/main/resources/data-dev.sql
@@ -0,0 +1,8 @@
+INSERT INTO list_domain (name) 
+VALUES ('ListOne'), ('ListTwo'), ('ListThree');
+
+INSERT INTO task_domain(description, is_done, my_list_id) 
+VALUES
+('TaskOne',false,1),
+('TaskTwo',false,2),
+('TaskThree',false,3);
