44a4ecba6c44f788b4f625bcfa4aa8afe04387b2
Celina <cbasa@qa.com>
2021-02-05 12:24:38 +0000

    Added TaskController

--- src/main/java/com/qa/demo/rest/TaskController.java
+++ src/main/java/com/qa/demo/rest/TaskController.java
@@ -2,8 +2,6 @@ package com.qa.demo.rest;
 
 import java.util.List;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,48 +14,48 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.qa.demo.persistence.domain.ListDomain;
-import com.qa.demo.persistence.dtos.ListDTO;
-import com.qa.demo.services.ListService;
+import com.qa.demo.persistence.domain.TaskDomain;
+import com.qa.demo.persistence.dtos.TaskDTO;
+import com.qa.demo.services.TaskService;
 
 @RestController
-@RequestMapping("/List")
-public class ListController {
+@RequestMapping("/Task")
+public class TaskController {
 
-	private ListService service;
+	private TaskService service;
 
 	@Autowired
-	public ListController(ListService service) {
+	public TaskController(TaskService service) {
 		super();
 		this.service = service;
 	}
 
 	// GET
 	@GetMapping("/readAll")
-	public ResponseEntity<List<ListDTO>> readAll() {
+	public ResponseEntity<List<TaskDTO>> readAll() {
 		return ResponseEntity.ok(this.service.readAll());
 	}
 
 	@GetMapping("/read/{id}")
-	public ResponseEntity<ListDTO> readList(@PathVariable("id") Long id) {
-		return ResponseEntity.ok(this.service.readList(id));
+	public ResponseEntity<TaskDTO> readTask(@PathVariable("id") Long id) {
+		return ResponseEntity.ok(this.service.readTask(id));
 	}
 
 	// POST
 	@PostMapping("/create")
-	public ResponseEntity<ListDTO> create(@RequestBody ListDomain list) {
-		return new ResponseEntity<ListDTO>(this.service.create(list), HttpStatus.CREATED);
+	public ResponseEntity<TaskDTO> create(@RequestBody TaskDomain task) {
+		return new ResponseEntity<TaskDTO>(this.service.create(task), HttpStatus.CREATED);
 	}
 
 	// PUT
-	@PutMapping("/update")
-	public ResponseEntity<ListDTO> updateList(@PathParam("id") Long id, @RequestBody ListDomain list) {
-		return new ResponseEntity<ListDTO>(this.service.update(id, list), HttpStatus.ACCEPTED);
+	@PutMapping("/update{id}")
+	public ResponseEntity<TaskDTO> updateTask(@PathVariable("id") Long id, @RequestBody TaskDomain task) {
+		return new ResponseEntity<TaskDTO>(this.service.update(id, task), HttpStatus.ACCEPTED);
 	}
 
 	// DELETE
 	@DeleteMapping("/delete/{id}")
-	public ResponseEntity<Object> deleteList(@PathVariable("id") Long id) {
+	public ResponseEntity<Object> deleteTask(@PathVariable("id") Long id) {
 		return this.service.delete(id) ? new ResponseEntity<>(HttpStatus.NO_CONTENT)
 				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
