76e8962f2314f7bc6b9bdb5046ed64d8c791b11d
Celina <cbasa@qa.com>
2021-02-05 16:17:49 +0000

    Added integration test for task controller read all and read task methods

--- src/main/resources/schema-test.sql
+++ src/main/resources/schema-test.sql
@@ -1,5 +1,5 @@
-drop table if exists list_domain
 drop table if exists task_domain
+drop table if exists list_domain
 create table list_domain (id bigint not null auto_increment, name varchar(255) not null, primary key (id)) engine=InnoDB
 create table task_domain (id bigint not null auto_increment, description varchar(255) not null, is_done bit not null, my_list_id bigint, primary key (id)) engine=InnoDB
 alter table task_domain add constraint FKjy3rnh62nume9qkpjui57vben foreign key (my_list_id) references list_domain (id) on delete cascade
