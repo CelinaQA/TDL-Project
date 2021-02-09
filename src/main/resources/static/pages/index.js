2e07fcd6897e3d5a45c42113d7c284984ff42951
Celina <cbasa@qa.com>
2021-02-09 21:46:05 +0000

    Added read list method to js file

---
+++ src/main/resources/static/pages/index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const displayList = document.querySelector("#displayList");
+const selectedList = document.querySelector("#selectList option:checked");
+
+console.log(selectedList);
+
+const printListToScreen = (description, isDone) => {
+    let listResult = document.createElement("p");
+    let text = document.createTextNode(`${description}`);
+    if (isDone == true) {
+        listResult.style = "text-decoration: line-through";
+    } else { };
+    listResult.appendChild(text);
+    displayList.appendChild(listResult);
+}
+
+const retrieveData = () => {
+    const idValue = selectedList.value;
+    let data = {
+        id: idValue
+    }
+    fetch(`http://localhost:8080/List/read/${data.id}`)
+        .then((response) => {
+            if (response.status !== 200) {
+                console.log(response);
+                throw new Error("I don't have a status of 200");
+            } else {
+                console.log(`response is OK (200)`);
+                //json-ify it (which returns a promise)
+                displayList.innerHTML = "";
+                response.json().then((infoList) => {
+                    console.log(infoList);
+                    console.log(infoList.taskList); // key - return array
+                    for (let task of infoList.taskList) {
+                        console.log(task.description);
+                        printListToScreen(task.description, task.isDone);
+                    }
+                })
+            }
+        }).catch((err) => {
+            console.error(err);
+        })
+}
