8e8fdc7b929261149f0dd1089a90013d4431a8d5
Celina <cbasa@qa.com>
2021-02-10 16:22:48 +0000

    Updated dropdown lists with existing lists

--- src/main/resources/static/pages/index.js
+++ src/main/resources/static/pages/index.js
@@ -1,18 +1,21 @@
 'use strict';
 
 const displayList = document.querySelector("#displayList");
-const selectedList = document.querySelector("#selectList");
+const selectList = document.querySelector("#selectList");
+const selectListAddTask = document.querySelector("#selectListAddTask");
+const alertMsg = document.querySelector("#onsuccess");
+const newListName = document.querySelector("#newListName");
 
 const createTab = document.querySelector("#create-tab");
 const readTab = document.querySelector("#read-tab");
 const updateTab = document.querySelector("#update-tab");
 const deleteTab = document.querySelector("#delete-tab");
 
-console.log(selectedList);
+console.log(selectList);
 
 const createList = () => {
 
-    const nameValue = inputName.value;
+    const nameValue = newListName.value;
 
     let data = {
         name: nameValue
@@ -46,8 +49,8 @@ const clearDisplayList = () => {
     displayList.innerHTML = "";
 }
 
-const retrieveData = () => {
-    const idValue = selectedList.value;
+const readList = () => {
+    const idValue = selectList.value;
     let data = {
         id: idValue
     }
@@ -74,5 +77,54 @@ const retrieveData = () => {
             }
         }).catch((err) => {
             console.error(err);
+            clearDisplayList();
+            alertMsg.setAttribute("class", "alert alert-danger");
+            alertMsg.innerHTML = "Please select a list!";
+            setTimeout(() => {
+                alertMsg.removeAttribute("class");
+                alertMsg.innerHTML = "";
+            }, 2000);
         })
 }
+
+const getAllList = () => {
+    fetch(`http://localhost:8080/List/readAll`)
+        .then((response) => {
+            if (response.status !== 200) {
+                console.log(response);
+                throw new Error("I don't have a status of 200");
+            } else {
+                console.log(`response is OK (200)`);
+                //json-ify it (which returns a promise)
+                response.json().then((infoList) => {
+                    console.log(infoList); // key - return array
+                    // selectListAddTask.innerHTML = "";
+                    // selectList.innerHTML = "";
+                    for (let list of infoList) {
+                        console.log(list.name);
+                        let listName = list.name;
+                        let listId = list.id;
+                        updateExistingLists(listName, listId);
+                    }
+                })
+            }
+        }).catch((err) => {
+            console.error(err);
+        })
+}
+
+
+const updateExistingLists = (listName, listId) => {
+    let text = document.createTextNode(listName);
+    let listOption = document.createElement("option")
+    listOption.value = `${listId}`;
+    listOption.appendChild(text);
+    let listOption1 = listOption.cloneNode(true);
+    selectListAddTask.appendChild(listOption);
+    selectList.appendChild(listOption1);
+    console.log(listOption)
+    console.log("--------------")
+    console.log(listOption1)
+}
+
+getAllList();
