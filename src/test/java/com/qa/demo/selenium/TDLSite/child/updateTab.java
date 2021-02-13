a7b7429b49183ab6e8877bf799af061d5afacaa8
Celina <cbasa@qa.com>
2021-02-13 16:18:54 +0000

    Added files for selenium testing

---
+++ src/test/java/com/qa/demo/selenium/TDLSite/child/updateTab.java
@@ -0,0 +1,5 @@
+package com.qa.demo.selenium.TDLSite.child;
+
+public class updateTab {
+
+}
--- src/test/java/com/qa/demo/selenium/TDLSiteTest.java
+++ src/test/java/com/qa/demo/selenium/TDLSiteTest.java
@@ -5,30 +5,35 @@ import java.util.Map;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import com.qa.demo.demosite.DemoSitePortal;
+import com.qa.demo.demosite.child.DemoLogin;
 
 public class TDLSiteTest {
-	
+
 	private static RemoteWebDriver driver;
-	private final String url = "http://thedemosite.co.uk/";
-	
+	private final String url = "http://127.0.0.1:8080/index.html";
+
 	// Designed to return ChromeOptions to configure new ChromeDrivers in Selenium
-		public static ChromeOptions chromeCfg() {
-			Map<String, Object> prefs = new HashMap<String, Object>();
-			ChromeOptions cOptions = new ChromeOptions();
+	public static ChromeOptions chromeCfg() {
+		Map<String, Object> prefs = new HashMap<String, Object>();
+		ChromeOptions cOptions = new ChromeOptions();
 
-			// Settings
-			prefs.put("profile.default_content_setting_values.cookies", 2);
-			prefs.put("network.cookie.cookieBehavior", 2);
-			prefs.put("profile.block_third_party_cookies", true);
+		// Settings
+		prefs.put("profile.default_content_setting_values.cookies", 2);
+		prefs.put("network.cookie.cookieBehavior", 2);
+		prefs.put("profile.block_third_party_cookies", true);
 
-			// Create ChromeOptions to disable Cookies pop-up
-			cOptions.setExperimentalOption("prefs", prefs);
+		// Create ChromeOptions to disable Cookies pop-up
+		cOptions.setExperimentalOption("prefs", prefs);
 
-			return cOptions;
-		}
+		return cOptions;
+	}
 
 	@BeforeAll
 	public static void beforeAll() {
@@ -38,13 +43,57 @@ public class TDLSiteTest {
 
 	}
 
+	@Test
+	public void createListTest() throws InterruptedException {
+
+		// GIVEN: that I can access http://thedemosite.co.uk/
+		driver.get(url);
+
+		DemoSitePortal website = PageFactory.initElements(driver, DemoSitePortal.class);
+		DemoLogin loginPage = PageFactory.initElements(driver, DemoLogin.class);
+
+	}
+
+	@Test
+	public void createTaskTest() throws InterruptedException {
+
+	}
+
+	@Test
+	public void readListTest() throws InterruptedException {
+
+	}
+
+	@Test
+	public void readTaskTest() throws InterruptedException {
+
+	}
+
+	@Test
+	public void updateListTest() throws InterruptedException {
+
+	}
+
+	@Test
+	public void updateTaskTest() throws InterruptedException {
+
+	}
+
+	@Test
+	public void deleteListTest() throws InterruptedException {
+
+	}
+
+	@Test
+	public void deleteTaskTest() throws InterruptedException {
+
+	}
+
 	@AfterAll
 	public static void afterAll() {
 
 		driver.quit();
 
 	}
-	
-	
 
 }
